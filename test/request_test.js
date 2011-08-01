463b50609de97a15db30dda52093485accf1f741
Michael Jackson <mjijackson@gmail.com>
2011-08-01 10:22:50 -0700

    More tests for Request

--- test/request_test.js
+++ test/request_test.js
@@ -6,6 +6,86 @@ var assert = require("assert"),
 
 vows.describe("request").addBatch({
     "A Request": {
+        topic: function () {
+            this.protocol = "https:";
+            this.hostname = "example.org";
+            this.pathname = "/some/path";
+            this.query = "a=1&b=hello%20world";
+            this.protocolVersion = "1.1";
+            this.method = "POST";
+            this.contentType = 'text/html; charset="utf-8"';
+            this.contentLength = "0";
+            this.userAgent = "test";
+            this.headers = {
+                "Content-Type": this.contentType,
+                "Content-Length": this.contentLength,
+                "User-Agent": this.userAgent,
+                "X-Requested-With": "XMLHttpRequest"
+            };
+
+            var env = mock.env({
+                protocol: this.protocol,
+                hostname: this.hostname,
+                pathname: this.pathname,
+                query: this.query
+            }, {
+                protocolVersion: this.protocolVersion,
+                method: this.method,
+                headers: this.headers
+            });
+
+            var req = new Request(env);
+
+            this.callback(null, req);
+        },
+        "should know its protocol": function (req) {
+            assert.equal(req.protocol, this.protocol);
+        },
+        "should know its protocol version": function (req) {
+            assert.equal(req.protocolVersion, this.protocolVersion);
+        },
+        "should know its method": function (req) {
+            assert.equal(req.method, this.method);
+        },
+        "should know its script name": function (req) {
+            assert.equal(req.scriptName, "");
+        },
+        "should be able to modify its script name": function (req) {
+            req.scriptName = "/another/path";
+            assert.equal(req.scriptName, "/another/path");
+        },
+        "should know its path info": function (req) {
+            assert.equal(req.pathInfo, this.pathname);
+        },
+        "should be able to modify its path info": function (req) {
+            req.pathInfo = "/another/path";
+            assert.equal(req.pathInfo, "/another/path");
+        },
+        "should know its query string": function (req) {
+            assert.equal(req.queryString, this.query);
+        },
+        "should be able to modify its query string": function (req) {
+            req.queryString = "a=2";
+            assert.equal(req.queryString, "a=2");
+        },
+        "should know its content type": function (req) {
+            assert.equal(req.contentType, this.contentType);
+        },
+        "should know its content length": function (req) {
+            assert.equal(req.contentLength, this.contentLength);
+        },
+        "should know its media type": function (req) {
+            assert.equal(req.mediaType, "text/html");
+        },
+        "should know its user agent": function (req) {
+            assert.equal(req.userAgent, this.userAgent);
+        },
+        "should know if it is secure": function (req) {
+            assert.ok(req.ssl);
+        },
+        "should know if it was made via XMLHttpRequest": function (req) {
+            assert.ok(req.xhr);
+        },
         "with cookies": {
             topic: function () {
                 this.cookie = "a=1, a=2,b=3";
--- test/utils_test.js
+++ test/utils_test.js
@@ -8,7 +8,9 @@ vows.describe("utils").addBatch({
     'A "Content-Type" header': headerContext("Content-Type", "Content-Type", "ContentType", "httpContentType"),
     'A "content-type" header': headerContext("content-type", "Content-Type", "ContentType", "httpContentType"),
     'An "X-Requested-With" header': headerContext("X-Requested-With", "X-Requested-With", "XRequestedWith", "httpXRequestedWith"),
-    'An "x-requested-with" header': headerContext("x-requested-with", "X-Requested-With", "XRequestedWith", "httpXRequestedWith")
+    'An "x-requested-with" header': headerContext("x-requested-with", "X-Requested-With", "XRequestedWith", "httpXRequestedWith"),
+    'An "X-Forwarded-SSL" header': headerContext("X-Forwarded-SSL", "X-Forwarded-Ssl", "XForwardedSsl", "httpXForwardedSsl"),
+    'An "x-forwarded-ssl" header': headerContext("x-forwarded-ssl", "X-Forwarded-Ssl", "XForwardedSsl", "httpXForwardedSsl")
 }).export(module);
 
 function headerContext(header, canonicalName, capitalizedName, propertyName) {
