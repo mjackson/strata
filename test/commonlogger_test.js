6d698a3e26556e08f7de7e7d2642cd78682fbf42
Michael Jackson <mjijackson@gmail.com>
2011-08-02 20:58:07 -0700

    Finished commonLogger and added tests

---
+++ test/commonlogger_test.js
@@ -0,0 +1,25 @@
+var assert = require("assert"),
+    vows = require("vows"),
+    mock = require("./../lib/link/mock"),
+    commonLogger = require("./../lib/link/commonlogger");
+
+vows.describe("commonlogger").addBatch({
+    "A commonLogger": {
+        topic: function () {
+            this.output = "";
+
+            var self = this;
+            var app = commonLogger(mock.empty, {
+                write: function (message) {
+                    self.output += message;
+                }
+            });
+
+            return app;
+        },
+        "should log the request": function (app) {
+            mock.request("", app, function (status, headers, body) {});
+            assert.match(this.output, /GET \/.+200/);
+        }
+    }
+}).export(module);
