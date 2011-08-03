2c8afff496ffca1f598dddcd4ac04baf0be915f8
Michael Jackson <mjijackson@gmail.com>
2011-08-02 21:07:24 -0700

    Finished contentLength and added tests

---
+++ test/contentlength_test.js
@@ -0,0 +1,40 @@
+var assert = require("assert"),
+    vows = require("vows"),
+    mock = require("./../lib/link/mock"),
+    contentLength = require("./../lib/link/contentlength");
+
+vows.describe("contentlength").addBatch({
+    "A contentLength": {
+        "should automatically add a Content-Length header": function () {
+            var body = "Hello world!";
+            var app = function (env, callback) {
+                callback(200, {}, body);
+            }
+
+            app = contentLength(app);
+
+            mock.request("", app, function (status, headers, body) {
+                assert.strictEqual(headers["Content-Length"], body.length.toString(10));
+            });
+        },
+        "should write notify link.error when being used on a stream": function () {
+            var errors = "";
+            var body = new mock.Stream("Hello world");
+            var app = function (env, callback) {
+                callback(200, {}, body);
+            }
+
+            app = contentLength(app);
+
+            mock.request({
+                error: {
+                    write: function (message) {
+                        errors += message;
+                    }
+                }
+            }, app, function (status, headers, body) {});
+
+            assert.match(errors, /body with no length/);
+        }
+    }
+}).export(module);
