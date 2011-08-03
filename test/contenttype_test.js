bac1540eaae5efc79638098ae845929576fe00f5
Michael Jackson <mjijackson@gmail.com>
2011-08-02 21:18:07 -0700

    Added tests for contentType

---
+++ test/contenttype_test.js
@@ -0,0 +1,21 @@
+var assert = require("assert"),
+    vows = require("vows"),
+    mock = require("./../lib/link/mock"),
+    contentType = require("./../lib/link/contenttype");
+
+vows.describe("contenttype").addBatch({
+    "A contentType": {
+        "should automatically add a Content-Type header": function () {
+            var type = "text/plain";
+            var app = function (env, callback) {
+                callback(200, {}, "");
+            }
+
+            app = contentType(app, type);
+
+            mock.request("", app, function (status, headers, body) {
+                assert.strictEqual(headers["Content-Type"], type);
+            });
+        }
+    }
+}).export(module);
