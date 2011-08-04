813b5ffbf61722d047905fcde64f810c8054e0d1
Michael Jackson <mjijackson@gmail.com>
2011-08-04 09:29:27 -0700

    Added tests for sessionCookie

---
+++ test/session_cookie_test.js
@@ -0,0 +1,112 @@
+var assert = require("assert"),
+    vows = require("vows"),
+    mock = require("./../lib/link/mock"),
+    sessionCookie = require("./../lib/link/session/cookie");
+
+vows.describe("session_cookie").addBatch({
+    "A sessionCookie middleware": {
+        "should properly serialize and deserialize cookie data": function () {
+            var sync = false;
+            var app = sessionCookie(increment);
+
+            mock.request("", app, function (status, headers, body) {
+                assert.ok(headers["Set-Cookie"]);
+                var match = headers["Set-Cookie"].match(/(link\.session=[^;]+)/);
+                assert.ok(match);
+                assert.equal(body, '{"counter":1}');
+
+                mock.request({
+                    headers: {
+                        "Cookie": match[1]
+                    }
+                }, app, function (status, headers, body) {
+                    sync = true;
+                    assert.ok(headers["Set-Cookie"]);
+                    assert.equal(body, '{"counter":2}');
+                });
+            });
+
+            assert.ok(sync);
+        },
+        "should reset the session when the cookie is tampered with": function () {
+            var sync = false;
+            var app = sessionCookie(increment);
+
+            mock.request("", app, function (status, headers, body) {
+                assert.ok(headers["Set-Cookie"]);
+                var match = headers["Set-Cookie"].match(/(link\.session=[^;]+)/);
+                assert.ok(match);
+                assert.equal(body, '{"counter":1}');
+
+                // Tamper with the cookie.
+                var cookie = match[1].substring(0, match[1].length - 2);
+
+                mock.request({
+                    headers: {
+                        "Cookie": cookie
+                    }
+                }, app, function (status, headers, body) {
+                    sync = true;
+                    assert.ok(headers["Set-Cookie"]);
+                    assert.equal(body, '{"counter":1}');
+                });
+            });
+
+            assert.ok(sync);
+        },
+        "should drop content when the cookie size exceeds 4k": function () {
+            var sync = false;
+            var errors = "";
+            var app = sessionCookie(toobig);
+
+            mock.request({
+                error: {
+                    write: function (message) {
+                        errors += message;
+                    }
+                }
+            }, app, function (status, headers, body) {
+                sync = true;
+                assert.isUndefined(headers["Set-Cookie"]);
+            });
+
+            assert.match(errors, /content dropped/i);
+            assert.ok(sync);
+        }
+    }
+}).export(module);
+
+function stringify(env, callback) {
+    var content = JSON.stringify(env["link.session"] || {});
+
+    callback(200, {
+        "Content-Type": "text/plain",
+        "Content-Length": content.length.toString(10)
+    }, content);
+}
+
+function increment(env, callback) {
+    assert.ok(env["link.session"]);
+
+    if (!("counter" in env["link.session"])) {
+        env["link.session"].counter = 0;
+    }
+
+    env["link.session"].counter += 1;
+
+    stringify(env, callback);
+}
+
+function toobig(env, callback) {
+    assert.ok(env["link.session"]);
+
+    var value = "";
+
+    for (var i = 0; i < 4096; ++i) {
+        value += "a";
+    }
+
+    env["link.session"].value = value;
+
+    stringify(env, callback);
+}
