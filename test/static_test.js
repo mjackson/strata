20c855af0fd0b1eecf71fba71b11b75af336f199
Michael Jackson <mjijackson@gmail.com>
2011-08-05 21:28:37 -0700

    Added index option to link.static

--- test/static_test.js
+++ test/static_test.js
@@ -6,43 +6,69 @@ var assert = require("assert"),
     mock = require("./../lib/link/mock"),
     stat = require("./../lib/link/static");
 
+var root = path.join(__dirname, "_files");
+
 vows.describe("static").addBatch({
     "A static middleware": {
         topic: function () {
-            this.root = path.join(__dirname, "_files");
-            this.text = fs.readFileSync(path.join(this.root, "text"), "utf8");
-
             var app = stat(function (env, callback) {
                 var message = "Not found: " + env.pathInfo;
                 callback(404, {
                     "Content-Type": "text/plain",
                     "Content-Length": message.length.toString(10)
                 }, message);
-            }, this.root);
+            }, root, "index.html");
 
             return app;
         },
-        "should correctly serve static files": function (app) {
-            var self = this;
-            mock.request("/text", app, function (status, headers, body) {
-                assert.equal(status, 200);
-                assert.instanceOf(body, EventEmitter);
-
-                var contents = "";
-
-                body.resume();
-                body.on("data", function (buffer) {
-                    contents += buffer.toString("utf8");
-                });
-                body.on("end", function () {
-                    assert.equal(contents, self.text);
-                });
-            });
+        "when a static file is requested": {
+            topic: function (app) {
+                this.body = fs.readFileSync(path.join(root, "text"), "utf8");
+                getBody("/text", app, this.callback);
+            },
+            "should serve that file": function (body) {
+                assert.equal(body, this.body);
+            }
         },
-        "should correctly forward the request to the downstream app when a matching file cannot be found": function (app) {
-            mock.request("/does-not-exist", app, function (status, headers, body) {
+        "when a directory is requested": {
+            topic: function (app) {
+                this.body = fs.readFileSync(path.join(root, "index.html"), "utf8");
+                getBody("/", app, this.callback);
+            },
+            "should serve the index file": function (body) {
+                assert.equal(body, this.body);
+            }
+        },
+        "when a matching file cannot be found": {
+            topic: function (app) {
+                getStatus("/does-not-exist", app, this.callback);
+            },
+            "should forward the request to the downstream app": function (status) {
                 assert.equal(status, 404);
-            });
+            }
         }
     }
 }).export(module);
+
+function getStatus(file, app, callback) {
+    mock.request(file, app, function (status, headers, body) {
+        callback(null, status);
+    });
+}
+
+function getBody(file, app, callback) {
+    mock.request(file, app, function (status, headers, body) {
+        assert.equal(status, 200);
+        assert.instanceOf(body, EventEmitter);
+
+        var contents = "";
+
+        body.resume();
+        body.on("data", function (buffer) {
+            contents += buffer.toString("utf8");
+        });
+        body.on("end", function () {
+            callback(null, contents);
+        });
+    });
+}
