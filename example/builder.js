7c76a8e2fd9da8bb1e6ba288ddd7f44118dac937
Michael Jackson <mjijackson@gmail.com>
2011-08-14 21:45:07 -0700

    Added Builder.route

--- example/builder.js
+++ example/builder.js
@@ -1,20 +1,48 @@
 var link = require("./../lib/link");
 
+/**
+ * This app is constructed using a link.Builder.
+ */
 var app = new link.Builder;
 
 app.use(link.commonLogger);
 app.use(link.contentType, "text/plain");
 app.use(link.contentLength);
 
-app.map("/examples", function (app) {
+/**
+ * This call mounts a static file server at the "/example" location. Any
+ * request to this location with a path to a file in this directory will serve
+ * that file.
+ */
+app.map("/example", function (app) {
     app.use(link.static, __dirname);
     app.run(function (env, callback) {
         callback(404, {}, "Not found: " + env.pathInfo);
     });
 });
 
+/**
+ * This route merely echoes back the request parameters as a JSON string in
+ * the request body. Works for all request methods.
+ */
+app.route("/params", function (env, callback) {
+    var req = new link.Request(env);
+
+    req.params(function (err, params) {
+        if (err && link.handleError(err, env, callback)) {
+            return;
+        }
+
+        callback(200, {}, "The request params were: " + JSON.stringify(params));
+    });
+});
+
+/**
+ * This is the default app which only gets called if none of the previous
+ * routes match.
+ */
 app.run(function (env, callback) {
-    callback(200, {}, "Try GET /examples/builder.js");
+    callback(200, {}, "Try GET /example/builder.js");
 });
 
 module.exports = app;
--- example/hello_builder.js
+++ example/hello_builder.js
@@ -3,7 +3,7 @@ var link = require("./../lib/link");
 /**
  * This app is constructed using a link.Builder. It uses several middlewares
  * in the request pipeline to automatically set some headers and log the
- * request. See builder.js for a more complex example of using a Builder.
+ * request.
  */
 var app = new link.Builder;
 
