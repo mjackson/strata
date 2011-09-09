7fa0c0bc6aab1f9668788900d2e89d4f38d02d1e
Michael Jackson <mjijackson@gmail.com>
2011-09-09 09:59:31 -0700

    Refactored some testing methods

--- test/static_test.js
+++ test/static_test.js
@@ -24,7 +24,7 @@ vows.describe("static").addBatch({
         "when a static file is requested": {
             topic: function (app) {
                 this.body = fs.readFileSync(path.join(root, "text"), "utf8");
-                getBody("/text", app, this.callback);
+                mock.getBody("/text", app, this.callback);
             },
             "should serve that file": function (body) {
                 assert.equal(body, this.body);
@@ -33,7 +33,7 @@ vows.describe("static").addBatch({
         "when a directory is requested": {
             topic: function (app) {
                 this.body = fs.readFileSync(path.join(root, "index.html"), "utf8");
-                getBody("/", app, this.callback);
+                mock.getBody("/", app, this.callback);
             },
             "should serve the index file": function (body) {
                 assert.equal(body, this.body);
@@ -41,7 +41,7 @@ vows.describe("static").addBatch({
         },
         "when a matching file cannot be found": {
             topic: function (app) {
-                getStatus("/does-not-exist", app, this.callback);
+                mock.getStatus("/does-not-exist", app, this.callback);
             },
             "should forward the request to the downstream app": function (status) {
                 assert.equal(status, 404);
@@ -49,26 +49,3 @@ vows.describe("static").addBatch({
         }
     }
 }).export(module);
-
-function getStatus(file, app, callback) {
-    mock.request(file, app, function (status, headers, body) {
-        callback(null, status);
-    });
-}
-
-function getBody(file, app, callback) {
-    mock.request(file, app, function (status, headers, body) {
-        assert.equal(status, 200);
-        assert.instanceOf(body, EventEmitter);
-
-        var contents = "";
-
-        body.resume();
-        body.on("data", function (buffer) {
-            contents += buffer.toString("utf8");
-        });
-        body.on("end", function () {
-            callback(null, contents);
-        });
-    });
-}
