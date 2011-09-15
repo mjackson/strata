2bf0f34fb98a7ea6650fc30f549a8870c67ca721
Michael Jackson <mjijackson@gmail.com>
2011-09-14 18:02:04 -0700

    Added rewrite middleware

---
+++ test/rewrite_test.js
@@ -0,0 +1,50 @@
+var assert = require("assert"),
+    vows = require("vows"),
+    strata = require("./../lib"),
+    mock = strata.mock,
+    rewrite = strata.rewrite;
+
+vows.describe("rewrite").addBatch({
+    "A rewrite middleware": {
+        topic: function () {
+            var app = function (env, callback) {
+                callback(200, {
+                    "Content-Type": "text/plain",
+                    "X-PathInfo": env.pathInfo
+                }, "");
+            }
+
+            app = rewrite(app, "/abc", "/xyz");
+            app = rewrite(app, /\/def/g, "/xyz");
+
+            return app;
+        },
+        "when /abc is requested": {
+            topic: function (app) {
+                mock.request("/abc", app, this.callback);
+            },
+            "should rewrite properly": function (err, status, headers, body) {
+                assert.ok(headers["X-PathInfo"]);
+                assert.equal(headers["X-PathInfo"], "/xyz");
+            }
+        },
+        "when /def is requested": {
+            topic: function (app) {
+                mock.request("/def", app, this.callback);
+            },
+            "should rewrite properly": function (err, status, headers, body) {
+                assert.ok(headers["X-PathInfo"]);
+                assert.equal(headers["X-PathInfo"], "/xyz");
+            }
+        },
+        "when /def/path/def is requested": {
+            topic: function (app) {
+                mock.request("/def/path/def", app, this.callback);
+            },
+            "should rewrite properly": function (err, status, headers, body) {
+                assert.ok(headers["X-PathInfo"]);
+                assert.equal(headers["X-PathInfo"], "/xyz/path/xyz");
+            }
+        }
+    }
+}).export(module);
