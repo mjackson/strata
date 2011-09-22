98ec5c9e12e33a6109c18b70286500645c8138de
Michael Jackson <mjijackson@gmail.com>
2011-09-22 09:16:32 -0700

    Refactored static middleware to be completely async
    Synchronous calls to exists and stat could block under heavy load.

--- test/static_test.js
+++ test/static_test.js
@@ -4,27 +4,23 @@ var assert = require("assert"),
     fs = require("fs"),
     EventEmitter = require("events").EventEmitter,
     mock = require("./../lib/mock"),
-    stat = require("./../lib/static");
+    stat = require("./../lib/static"),
+    utils = require("./../lib/utils");
 
 var root = path.join(__dirname, "_files");
 
 vows.describe("static").addBatch({
     "A static middleware": {
         topic: function () {
-            var app = stat(function (env, callback) {
-                var message = "Not found: " + env.pathInfo;
-                callback(404, {
-                    "Content-Type": "text/plain",
-                    "Content-Length": message.length.toString(10)
-                }, message);
-            }, root, "index.html");
-
-            return app;
+            return stat(utils.notFound, root, "index.html");
         },
         "when a static file is requested": {
             topic: function (app) {
                 this.body = fs.readFileSync(path.join(root, "text"), "utf8");
                 mock.request("/text", app, this.callback);
+            },
+            "should return a 200": function (err, status, headers, body) {
+                assert.equal(status, 200);
             },
             "should serve that file": function (err, status, headers, body) {
                 assert.equal(body, this.body);
@@ -37,6 +33,9 @@ vows.describe("static").addBatch({
             topic: function (app) {
                 this.body = fs.readFileSync(path.join(root, "index.html"), "utf8");
                 mock.request("/", app, this.callback);
+            },
+            "should return a 200": function (err, status, headers, body) {
+                assert.equal(status, 200);
             },
             "should serve the index file": function (err, status, headers, body) {
                 assert.equal(body, this.body);
@@ -51,6 +50,26 @@ vows.describe("static").addBatch({
             },
             "should forward the request to the downstream app": function (err, status, headers, body) {
                 assert.equal(status, 404);
+            }
+        }
+    },
+    "A static middleware with multiple index files": {
+        topic: function () {
+            return stat(utils.notFound, root, ["index.htm", "index.html"]);
+        },
+        "when a directory is requested": {
+            topic: function (app) {
+                this.body = fs.readFileSync(path.join(root, "index.html"), "utf8");
+                mock.request("/", app, this.callback);
+            },
+            "should return a 200": function (err, status, headers, body) {
+                assert.equal(status, 200);
+            },
+            "should serve the first index file that exists": function (err, status, headers, body) {
+                assert.equal(body, this.body);
+            },
+            "should set the correct Content-Type": function (err, status, headers, body) {
+                assert.equal(headers["Content-Type"], "text/html");
             }
         }
     }
